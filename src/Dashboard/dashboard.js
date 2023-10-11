6dab7d8edfa517f4f3ea27eb85aaaa9f801fc98c
Irfan_UI <irfan.alam@greenjeeva.com>
2023-10-12 00:32:38 +0530

    dashboard and protection file

---
+++ src/Dashboard/dashboard.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Dashboard = ({ loggedInUsername }) => {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Welcome, {loggedInUsername}!</p> {/* Display the username here */}
+      <p>Welcome to your dashboard! Here, you can add any content or functionality you need for your logged-in users.</p>
+      {/* Add more components, features, or data here */}
+    </div>
+  );
+};
+
+export default Dashboard;

83a453ef21b41982eeb7a6c13bb351a5e473ba86
Irfan_UI <irfan.alam@greenjeeva.com>
2023-10-25 14:58:16 +0530

    two api home component

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -9,7 +9,7 @@ const Home = ({ onAddToCart }) => {
   const [products, setProducts] = useState([]); /*const [products, setProducts] = useState([]);: 
   This line declares a state variable products and a function setProducts to update it using the 
   useState hook. The initial value of products is an empty array [].*/
-
+  const [newArrivals, setNewArrivals] = useState([]);
   useEffect(() => {
     // Fetch data from the API
     axios.get('http://localhost:3000/getproducts')
@@ -20,7 +20,16 @@ const Home = ({ onAddToCart }) => {
       .catch((error) => {
         console.error('Error fetching data:', error);
       });
+      axios.get('https://fakestoreapi.com/products')
+      .then((res) => {
+        console.log('Data from the second API:', res.data);
+        setNewArrivals(res.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
   }, []);
+
 
   const handleAddToCart = (product) => {
     onAddToCart(product);
@@ -29,31 +38,61 @@ const Home = ({ onAddToCart }) => {
 
   return (
     <div>
-      <h1>Products</h1>
-      <div className="product-grid">
-        {products.map((product, index) => (
-          <div key={index} className="product-card">
-                      console.log(product);
+      <div>
+        <h1 className="title">Mobile </h1>
+        <div className="product-grid">
+          {products.map((product, index) => (
+            <div key={index} className="product-card">
 
-            <img
-              src={`http://localhost:3000/${product.product_image}`}
-              alt={product.name}
-              className="product-image"
-            />
-            <h3>{product.name}</h3>
-            <p>Price: ${parseFloat(product.price).toFixed(2)}</p>
-            <div className="button-container">
-              <button
-                className="add-to-cart-button"
-                onClick={() => handleAddToCart(product)}
-              >
-                Add to Cart
-              </button>
-              <button className="buy-button">Buy</button>
+              <img
+                src={`http://localhost:3000/${product.product_image}`}
+                alt={product.name}
+                className="product-image"
+                style={{ width: "200px", height: "200px" }}
+              />
+              <h3>{product.name}</h3>
+              <p>Price: ${parseFloat(product.price).toFixed(2)}</p>
+              <div className="button-container">
+                <button
+                  className="add-to-cart-button"
+                  onClick={() => handleAddToCart(product)}
+                >
+                  Add to Cart
+                </button>
+                <button className="buy-button">Buy</button>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
+      <div>
+        <h1>New Arrival</h1>
+        <div className="product-grid">
+          {newArrivals.map((product, index) => (
+            <div key={index} className="product-card">
+              <img
+                src={product.image}
+                alt={product.title}
+                className="product-image"
+                style={{ width: "200px", height: "200px" }}
+              />
+              <h3>{product.category}</h3>
+              <p>{product.title}</p>
+              <p>Price: ${parseFloat(product.price).toFixed(2)}</p>
+              <p>{product.rating.rate}</p>
+              <div className="button-container">
+                <button
+                  className="add-to-cart-button"
+                  onClick={() => handleAddToCart(product)}
+                >
+                  Add to Cart
+                </button>
+                <button className="buy-button">Buy</button>
+              </div>
+            </div>
+          ))}
+        </div>
+    </div>
     </div>
   );
 };
